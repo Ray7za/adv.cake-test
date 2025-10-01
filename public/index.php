e172402f2f47404cf67348265300e9eaee387207
Robert Cerepinschii <rob97@mail.ru>
2025-10-01 21:29:28 +0300

    Update docker adn index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-echo stingReverse('ToM, Anna and Alex can`t go to day-party on 9 july');
+$string = 'ToM, Anna and Alex can`t go to day-party on 9 july';
+
+echo 'Original: ' . $string;
+echo  'Reversed: '.stingReverse($string);
