e9364dd943f3c79d224425b3a66ca515035653c2
Robert Cerepinschii <rob97@mail.ru>
2025-10-01 21:30:04 +0300

    Update docker and index

--- public/index.php
+++ public/index.php
@@ -3,5 +3,6 @@ require __DIR__.'/../vendor/autoload.php';
 
 $string = 'ToM, Anna and Alex can`t go to day-party on 9 july';
 
+
 echo 'Original: ' . $string;
 echo  'Reversed: '.stingReverse($string);
