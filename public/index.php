a91800a419df58b98128eae31addf04360207ee0
Robert Cerepinschii <rob97@mail.ru>
2025-10-01 21:48:36 +0300

    Update index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 require __DIR__.'/../vendor/autoload.php';
 
 $string = 'ToM, Anna and Alex can`t go to day-party on 9 july';
+$reversed = stingReverse($string);
 
 
-echo 'Original: ' . $string;
-echo  'Reversed: '.stingReverse($string);
+echo 'Original: ' . $string.' <br>';
+echo  'Reversed: '.$reversed;
